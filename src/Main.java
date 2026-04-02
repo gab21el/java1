1486a1e28704eba430e061e0300a4365bfe4cd54
gab21el <gabrieloliveirassantos@gmail.com>
2026-04-02 15:24:09 -0300

    código que pede informações

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,23 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        int answer = sum(4, 3, 6, 1);
-        System.out.println("Sum: " + answer);
+    public static void main (String [] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Boas vindas! Insira seus dados no console: ");
+        System.out.println("Nome completo: ");
+        String nome = scanner.nextLine();
+        System.out.println("CPF: ");
+        String cpf = scanner.nextLine();
+        System.out.println("Idade: ");
+        int idade = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Altura: ");
+        int altura = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Estado civil: ");
+        String ec = scanner.nextLine();
+        System.out.println("Seu nome é "+ nome + "\nSeu cpf " + cpf + "\nsua idade " + idade + "\nsua altura " + altura + "\ne seu estado civil é " + ec);
 
-
-
-
-        }
-    public static int sum(int number1, int number2, int number3, int number4) {
-        return number1+number2+number3+number4;
 
     }
-
 }
